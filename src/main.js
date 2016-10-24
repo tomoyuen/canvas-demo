3c50d7807025e11f392ce78c91d13ba0e118ddeb
tomoyuen <wisyue@yahoo.com>
2016-10-24 15:53:51 +0800

    fix bugs && change interface;

--- src/main.js
+++ src/main.js
@@ -9,15 +9,21 @@
 
 import imgUrl from './assets/images/demo.jpg';
 
-const dotList = [];
+var dotList = [];
 var finishCount = 0;
-var ctx = null;
 var rafId = null;
 var fontSize = 500;
 var fontFamily = 'Helvetica Neue, Helvetica, Arial, sans-serif';
 
 const img = new window.Image();
 img.src = imgUrl;
+
+const input = window.document.querySelector('#word');
+const canvas = window.document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.document.body.clientWidth;
+canvas.height = window.document.body.clientHeight;
 
 class Dot {
   constructor(centerX, centerY, radius) {
@@ -26,8 +32,8 @@ class Dot {
     this.radius = radius;
     this.frameNum = 0;
     this.frameCount = Math.ceil(3000 / 16.66);
-    this.sx = 400;
-    this.sy = 400;
+    this.sx = 1000;
+    this.sy = 900;
     this.delay = this.frameCount * Math.random();
     this.delayCount = 0;
   }
@@ -41,10 +47,9 @@ function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-function handleCanvas(canvas) {
+function handleCanvas() {
+  dotList = [];
   const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  console.log(imgData);
 
   for (let x = 0; x < imgData.width; x += 6) {
     for (let y = 0; y < imgData.height; y += 6) {
@@ -83,7 +88,7 @@ function drawAnimate() {
   var curY;
   rafId = window.requestAnimationFrame(drawAnimate);
 
-  ctx.clearRect(0, 0, window.document.body.clientWidth, window.document.body.clientHeight);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = '#000';
 
   finishCount = 0;
@@ -100,6 +105,7 @@ function drawAnimate() {
     if (item.frameNum < item.frameCount) {
       curX = easeInOutCubic(item.frameNum, item.sx, item.x - item.sx, item.frameCount);
       curY = easeInOutCubic(item.frameNum, item.sy, item.y - item.sy, item.frameCount);
+      // console.log(`${curX} ${curY}`);
 
       ctx.arc(curX, curY, item.radius, 0, 2 * Math.PI);
       item.frameNum += 1;
@@ -123,13 +129,6 @@ function init() {
   // const imgW = img.width;
   // const imgH = img.height;
   let size = 0;
-  const input = window.document.querySelector('#word');
-
-  const canvas = window.document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
-
-  canvas.width = window.document.body.clientWidth;
-  canvas.height = window.document.body.clientHeight;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   const word = input.value ? input.value : 'beta';
@@ -149,7 +148,7 @@ function init() {
   ctx.textBaseline = 'top';
   ctx.fillText(word, 10, 0);
 
-  handleCanvas(canvas);
+  handleCanvas();
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -165,4 +164,7 @@ if (img.complete) {
   };
 }
 
-window.document.querySelector('#btn').onclick = init;
+window.document.querySelector('.input-block').onsubmit = (event) => {
+  event.preventDefault();
+  init();
+};
