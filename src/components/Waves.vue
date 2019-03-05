3aadddc8bcf8e8dd6b225815905b8281dc2e282a
tomoyuen <wisyue@yahoo.com>
2019-03-05 18:15:38 +0800

    add circle sprite map for particles re #3

--- src/components/Waves.vue
+++ src/components/Waves.vue
@@ -24,124 +24,137 @@
 </style>
 <script>
   /* global THREE */
-  // import * as THREE from 'three';
+  const SEPARATION = 100,
+    AMOUNTX = 100,
+    AMOUNTY = 70;
 
-  window.THREE = THREE;
+
+  var container;
+  var camera,
+    scene,
+    renderer;
+
+  var particles,
+    particle,
+    count = 0;
+
+  var mouseX = 85,
+    mouseY = -342;
+
+  var windowHalfX = window.innerWidth / 2;
+  var windowHalfY = window.innerHeight / 2;
+
+  function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  function onDocumentMouseMove(event) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+  }
+
+  function onDocumentTouchStart(event) {
+    if (event.touches.length === 1) {
+      event.preventDefault();
+      mouseX = event.touches[0].pageX - windowHalfX;
+      mouseY = event.touches[0].pageY - windowHalfY;
+    }
+  }
+
+  function onDocumentTouchMove(event) {
+    if (event.touches.length === 1) {
+      event.preventDefault();
+      mouseX = event.touches[0].pageX - windowHalfX;
+      mouseY = event.touches[0].pageY - windowHalfY;
+    }
+  }
+
+  function makeSprite() {
+    const PI2 = Math.PI * 2;
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const spriteSize = 8;
+    canvas.width = canvas.height = spriteSize * 2;
+    ctx.fillStyle = '#fff';
+    ctx.beginPath();
+    ctx.arc(spriteSize, spriteSize, spriteSize, 0, PI2, true);
+    ctx.fill();
+
+    const sprite = new THREE.Texture(canvas);
+    sprite.needsUpdate = true;
+
+    return sprite;
+  }
+
+  function init() {
+    camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    particles = [];
+
+    const material = new THREE.SpriteMaterial({ map: makeSprite(), color: 0xe1e1e1 });
+    const sprite = new THREE.Sprite(material);
+    sprite.scale.set(0.6, 0.6);
+
+    let i = 0;
+
+    for (let ix = 0; ix < AMOUNTX; ix++) {
+      for (let iy = 0; iy < AMOUNTY; iy++) {
+        particle = particles[i++] = sprite.clone();
+        particle.position.x = (ix * SEPARATION) - ((AMOUNTX * SEPARATION) / 2);
+        particle.position.z = (iy * SEPARATION) - ((AMOUNTY * SEPARATION) / 2);
+        scene.add(particle);
+      }
+    }
+
+    container = document.querySelector('#waves');
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('touchstart', onDocumentTouchStart, false);
+    document.addEventListener('touchmove', onDocumentTouchMove, false);
+
+    window.addEventListener('resize', onWindowResize, false);
+  }
+
+  function render() {
+    camera.position.x += (mouseX - camera.position.x) * 0.05;
+    camera.position.y += (-mouseY - camera.position.y) * 0.05;
+    camera.lookAt(scene.position);
+
+    let i = 0;
+
+    for (let ix = 0; ix < AMOUNTX; ix++) {
+      for (let iy = 0; iy < AMOUNTY; iy++) {
+        particle = particles[i++];
+        particle.position.y = (Math.sin((ix + count) * 0.3) * 50)
+          + (Math.sin((iy + count) * 0.5) * 50);
+        particle.scale.x = particle.scale.y = ((Math.sin((ix + count) * 0.3) + 1) * 2)
+          + ((Math.sin((iy + count) * 0.5) + 1) * 2);
+      }
+    }
+    renderer.render(scene, camera);
+    count += 0.1;
+  }
+
+  function animate() {
+    window.requestAnimationFrame(animate);
+    render();
+  }
 
   export default {
     mounted() {
-      const SEPARATION = 100,
-        AMOUNTX = 100,
-        AMOUNTY = 70;
-
-      var container;
-      var camera,
-        scene,
-        renderer;
-
-      var particles,
-        particle,
-        count = 0;
-
-      var mouseX = 85,
-        mouseY = -342;
-
-      var windowHalfX = window.innerWidth / 2;
-      var windowHalfY = window.innerHeight / 2;
-
-      function onWindowResize() {
-        windowHalfX = window.innerWidth / 2;
-        windowHalfY = window.innerHeight / 2;
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-      }
-
-      function onDocumentMouseMove(event) {
-        mouseX = event.clientX - windowHalfX;
-        mouseY = event.clientY - windowHalfY;
-      }
-
-      function onDocumentTouchStart(event) {
-        if (event.touches.length === 1) {
-          event.preventDefault();
-          mouseX = event.touches[0].pageX - windowHalfX;
-          mouseY = event.touches[0].pageY - windowHalfY;
-        }
-      }
-
-      function onDocumentTouchMove(event) {
-        if (event.touches.length === 1) {
-          event.preventDefault();
-          mouseX = event.touches[0].pageX - windowHalfX;
-          mouseY = event.touches[0].pageY - windowHalfY;
-        }
-      }
-
-      function init() {
-        camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 1, 10000);
-        camera.position.z = 1000;
-
-        scene = new THREE.Scene();
-
-        window.scene = scene;
-
-        particles = [];
-
-        const material = new THREE.SpriteMaterial({ color: 0xe1e1e1 });
-        const sprite = new THREE.Sprite(material);
-        sprite.scale.set(0.6, 0.6);
-
-        let i = 0;
-
-        for (let ix = 0; ix < AMOUNTX; ix++) {
-          for (let iy = 0; iy < AMOUNTY; iy++) {
-            particle = particles[i++] = sprite.clone();
-            particle.position.x = (ix * SEPARATION) - ((AMOUNTX * SEPARATION) / 2);
-            particle.position.z = (iy * SEPARATION) - ((AMOUNTY * SEPARATION) / 2);
-            scene.add(particle);
-          }
-        }
-
-        container = document.querySelector('#waves');
-        renderer = new THREE.WebGLRenderer({ alpha: true });
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        container.appendChild(renderer.domElement);
-
-        document.addEventListener('mousemove', onDocumentMouseMove, false);
-        document.addEventListener('touchstart', onDocumentTouchStart, false);
-        document.addEventListener('touchmove', onDocumentTouchMove, false);
-
-        window.addEventListener('resize', onWindowResize, false);
-      }
-
-      function render() {
-        camera.position.x += (mouseX - camera.position.x) * 0.05;
-        camera.position.y += (-mouseY - camera.position.y) * 0.05;
-        camera.lookAt(scene.position);
-
-        let i = 0;
-
-        for (let ix = 0; ix < AMOUNTX; ix++) {
-          for (let iy = 0; iy < AMOUNTY; iy++) {
-            particle = particles[i++];
-            particle.position.y = (Math.sin((ix + count) * 0.3) * 50)
-              + (Math.sin((iy + count) * 0.5) * 50);
-            particle.scale.x = particle.scale.y = ((Math.sin((ix + count) * 0.3) + 1) * 2)
-              + ((Math.sin((iy + count) * 0.5) + 1) * 2);
-          }
-        }
-        renderer.render(scene, camera);
-        count += 0.1;
-      }
-
-      function animate() {
-        window.requestAnimationFrame(animate);
-        render();
-      }
-
       init();
       animate();
     },
